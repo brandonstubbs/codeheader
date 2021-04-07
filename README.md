e8c8be72c65b158c079dbd28b1571b9acb3a0c3f
Brandon Stubbs <brandon@bos-solutions.co.uk>
2021-04-06 21:08:30 -0400

    project init

---
+++ README.md
@@ -0,0 +1,21 @@
+# codeheader
+
+## Features
+
+This extension allows you to quickly insert commented headings within your code.
+This extension is a work in progress and should not be used yet...
+
+TODO - add gif
+
+## Keybindings
+
+By default the `ctrl + i ctrl + c` keybinding is used. This executes the
+`CodeHeader` function which will turn your text into a code comment heading.
+
+## Release Notes
+
+Below are the releases for the application
+
+### 0.0.1
+
+First ever release, very basic. Something working.
---
+++ src/extension.ts
@@ -0,0 +1,65 @@
+import * as vscode from "vscode";
+
+const languageComment = new Map([
+  // language particular comments
+  ["terraform", "#"],
+]);
+
+export function activate(context: vscode.ExtensionContext) {
+  let codeheader = vscode.commands.registerCommand(
+    "codeheader.comment",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return; // There is no active editor, we have nothing to do 😢
+      }
+
+      const document = editor.document;
+
+      let selection = editor.selection;
+      if (selection.isEmpty) {
+        let currentLineStart = new vscode.Position(selection.active.line, 0);
+        let currentLineEnd = new vscode.Position(
+          selection.active.line,
+          document.lineAt(selection.active.line).range.end.character
+        );
+        selection = new vscode.Selection(currentLineStart, currentLineEnd);
+      } else {
+        selection = editor.selection;
+      }
+
+      const title = document.getText(selection);
+      const rulerConfig:
+        | Array<number>
+        | undefined = vscode.workspace.getConfiguration("editor").get("rulers");
+
+      const sym = languageComment.get(document.languageId) || ";";
+      const dblSym = sym + sym;
+      const normTitle = title.toUpperCase().split("").join(" ");
+      const decorTitle = "----==| " + normTitle + " |==----";
+      const lineLength = rulerConfig ? rulerConfig[0] : 80;
+      const spaceL = Math.floor((lineLength - decorTitle.length - 4) / 2);
+      const spaceR = Math.ceil((lineLength - decorTitle.length - 4) / 2);
+
+      // prettier-ignore
+      const codeComment =
+			 	sym.repeat(lineLength) + "\n" +
+			 	dblSym + " ".repeat(lineLength - 4) + dblSym + "\n" +
+			 	dblSym + " ".repeat(spaceL) + decorTitle + " ".repeat(spaceR) + dblSym + "\n" +
+				dblSym + " ".repeat(lineLength - 4) + dblSym + "\n" +
+			  sym.repeat(lineLength) + "\n";
+
+      editor
+        .edit((editBuilder) => {
+          editBuilder.replace(selection, codeComment);
+        })
+        .then((success) => {
+          // We remove any highlighting over the text
+          let position = editor.selection.end;
+          editor.selection = new vscode.Selection(position, position);
+        });
+    }
+  );
+
+  context.subscriptions.push(codeheader);
+}
